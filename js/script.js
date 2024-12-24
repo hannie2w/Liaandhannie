5954e2f32fe99bb746d9fb7a37b9cf28b6c7083c
Liya-Alexandrovna <liakulesova83096@gmail.com>
2024-12-24 17:53:25 +0300

    hannie2w
    изм

--- js/script.js
+++ js/script.js
@@ -1,40 +1,31 @@
 const app = document.getElementById('api')
 const temp = document.getElementById('temp')
 const text = document.getElementById('text')
-const d = document.getElementById('d')
 const icon = document.getElementById('icon')
 const week = document.getElementById('week')
-const day = document.getElementById('day')
+const day = document.getElementById('hour')
 const windowWeather = document.getElementById('window')
-
-
 
 const input = document.querySelector('.city')
 const search = document.querySelector('.search')
 
 search.addEventListener('click', () => {
     city = input.value.trim()
-    console.log(city)
 
     const apiKey = 'd29db7276247491f816132839242212'
-
     const api = `http://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=7`
 
     fetch(api).then((response)=> {
         return response.json()
     }).then(async (data) => {
-        console.log(data)
         const{ 
             current: {temp_c: temperature, condition},
             location: {name},
             forecast: {forecastday} 
         } = data
-        console.log(condition.icon)
         icon.innerHTML = '<img src="https://' + condition.icon + '"/>'
         text.innerHTML = name  
         temp.innerHTML = Math.round(temperature)+'°С'
-        console.log(forecastday[0].day.avgtemp_c);
-        d.innerHTML = forecastday[0].day.avgtemp_c
         try {
             let windowImage = "" ;
 
@@ -56,57 +47,43 @@ search.addEventListener('click', () => {
         } catch (error) {
             console.error('Error fetching weather data:', error);  
         }
-        await updateForecastHourInfo(name,forecastday)
-        await updateForecastInfo(name,forecastday)
+        await updateForecastHourInfo(forecastday)
+        await updateForecastInfo(forecastday)
     })
-
-})
-
-
-
-
-
-async function updateForecastInfo(name,forecastday) {
-    const  forecastData = forecastday
-    const timeTaken = '12:00:00'
-    const todayDate = new Date().toISOString().split('T')[0]
+})                                      
+async function updateForecastInfo(forecastday) {
+    const forecastData = forecastday
 
     week.innerHTML = ''
+    
     forecastData.forEach(el => {
         const date = new Date(Date.parse(el.date))
-        const forecastItem = `<div class="conteiner">
+        const forecastItem = `<div class="container">
         <ul>
             <li>${date.getDate() + '.' + (date.getMonth()+1)}</li>
             <li><img src="https://${el.day.condition.icon}"/></li>
-            <li id="d">${Math.round(el.day.avgtemp_c)}°С</li>
+            <li>${Math.round(el.day.avgtemp_c)}°С</li>
         </ul> 
-    </div>
-        `
+    </div>`
         week.insertAdjacentHTML('beforeend',forecastItem)
     })  
 }
-
-async function updateForecastHourInfo(name,forecastday) {
-    const  forecastData = forecastday[0]
+async function updateForecastHourInfo(forecastday) {
+    const forecastData = forecastday[0]
     
-    const timeTaken = '12:00:00'
-    const todayDate = new Date().toISOString().split('T')[0]
     week.innerHTML = ''
     day.innerHTML = ''
 
-    const date = new Date(Date.parse(forecastData.date))
     const forecastHours = forecastData.hour
     forecastHours.forEach(hour => {
         const time = new Date(Date.parse(hour.time))
-        const forecastItemHour = `
-        <div class="time" id="start" >
+        const forecastItemHour = `<div class="content">
                     <ul>
                         <li class="t" >${time.getHours() + ':' + time.getMinutes()+'0'}</li>
                         <li class="t"><img src="https://${hour.condition.icon}"/></li>
                         <li class="t">${Math.round(hour.temp_c)}°С</li>
                     </ul>
                 </div>`
-
     day.insertAdjacentHTML('beforeend',forecastItemHour)
  })
 }
